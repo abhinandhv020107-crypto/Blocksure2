a1d84428ab5200642efa3b608bc7f83d40c9ae8c
abhinandhv020107-crypto <abhinandhv020107@gmail.com>
2026-07-18 22:04:25 +0530

    Delete routes directory

--- routes/admin_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-admin_bp = Blueprint("admin", __name__)
-
-@admin_bp.route("/admin-dashboard")
-def dashboard():
-    return render_template("admin_dashboard.html")
