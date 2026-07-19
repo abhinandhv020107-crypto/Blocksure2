df28413533faffe516fa00ecd8ec66c660553457
abhinandhv020107-crypto <abhinandhv020107@gmail.com>
2026-07-19 08:05:49 +0530

    Updated QR generator

--- qr_module/qr_generator.py
+++ qr_module/qr_generator.py
@@ -1,13 +1,19 @@
 import os
 import qrcode
 
-QR_FOLDER=os.path.join("static","qr_codes")
-os.makedirs(QR_FOLDER,exist_ok=True)
+QR_FOLDER = os.path.join("static", "qr_codes")
+os.makedirs(QR_FOLDER, exist_ok=True)
+
 
 def generate_qr(product_code):
-    data=f"PRODUCT:{product_code}"
-    filename=f"{product_code}.png"
-    path=os.path.join(QR_FOLDER,filename)
-    img=qrcode.make(data)
+
+    # CHANGE THIS IP TO YOUR LAPTOP'S IPv4 ADDRESS
+    verification_url = f"http://192.168.167.1:5000/verify/{product_code}"
+
+    filename = f"{product_code}.png"
+    path = os.path.join(QR_FOLDER, filename)
+
+    img = qrcode.make(verification_url)
     img.save(path)
+
     return path
